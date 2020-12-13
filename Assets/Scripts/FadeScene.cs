43cfc6bc32f1113a4c42e7098d8786e990a19858
Kaan <mkaanalper@hotmail.com>
2020-12-13 11:49:50 +0300

    SceneSettings
    Guns, UI's, Mechanics and Scene Management

---
+++ Assets/Scripts/FadeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeScene : MonoBehaviour
+{
+
+    public Image blackFade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackFade.canvasRenderer.SetAlpha(0.0f);
+
+        fadeIn();
+    }
+
+    // Update is called once per frame
+    void fadeIn()
+    {
+        blackFade.CrossFadeAlpha(1, 5, false);
+    }
+}
